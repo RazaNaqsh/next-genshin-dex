a6774632c8b324dcce3d5cff97e2f3b307c5e9f7
RazaNaqsh <owaisraza1704@gmail.com>
2023-06-17 18:30:09 +0530

    small changes in nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,38 +4,42 @@ import React from "react";
 
 const Navbar = () => {
 	return (
-		<nav className="flex h-fit bg-[#000000] px-4 items-center">
-			<Link href="/">
-				<Image
-					src="/genshinLogo.png"
-					height={100}
-					width={130}
-					alt="logo"
-					className="antialiased mr-4"
-				/>
-			</Link>
-			<Link
-				href="/"
-				className="text-sm px-7 py-4 hover:bg-blue-300 hover:text-zinc-900 font-extralight h-full"
-			>
-				Dex
-			</Link>
-			<Link
-				href="/"
-				className="text-sm px-7 py-4 hover:bg-blue-300 hover:text-zinc-900 font-extralight h-full"
-			>
-				Explore
-			</Link>
-			<Link
-				href="/"
-				className="text-sm px-7 py-4 hover:bg-blue-300 hover:text-zinc-900 font-extralight h-full"
-			>
-				Github
-			</Link>
+		<nav className="flex h-fit justify-between bg-[#000000] px-6 items-center">
+			<div className="flex">
+				<Link href="/">
+					<Image
+						src="/genshinLogo.png"
+						height={100}
+						width={130}
+						alt="logo"
+						className="antialiased mr-4"
+					/>
+				</Link>
+				<Link
+					href="/"
+					className="text-sm px-7 py-4 hover:bg-blue-300 hover:text-zinc-900 font-extralight h-full"
+				>
+					Dex
+				</Link>
+				<Link
+					href="/"
+					className="text-sm px-7 py-4 hover:bg-blue-300 hover:text-zinc-900 font-extralight h-full"
+				>
+					Explore
+				</Link>
+				<Link
+					href="/"
+					className="text-sm px-7 py-4 hover:bg-blue-300 hover:text-zinc-900 font-extralight h-full"
+				>
+					Github
+				</Link>
+			</div>
 
-			<button className="text-sm px-7 py-3 rounded-full bg-blue-300  font-extralight h-full">
-				Sign Up
-			</button>
+			<div>
+				<button className="text-sm px-12 py-3 rounded-full bg-blue-300 text-black mr-4 font-extralight h-full">
+					Sign Up
+				</button>
+			</div>
 		</nav>
 	);
 };
