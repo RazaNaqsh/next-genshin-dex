e920d5562e33eac914ad9933252f1de042ace8f8
RazaNaqsh <owaisraza1704@gmail.com>
2023-06-17 18:27:16 +0530

    Navbvbar added

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+	return (
+		<nav className="flex h-fit bg-[#000000] px-4 items-center">
+			<Link href="/">
+				<Image
+					src="/genshinLogo.png"
+					height={100}
+					width={130}
+					alt="logo"
+					className="antialiased mr-4"
+				/>
+			</Link>
+			<Link
+				href="/"
+				className="text-sm px-7 py-4 hover:bg-blue-300 hover:text-zinc-900 font-extralight h-full"
+			>
+				Dex
+			</Link>
+			<Link
+				href="/"
+				className="text-sm px-7 py-4 hover:bg-blue-300 hover:text-zinc-900 font-extralight h-full"
+			>
+				Explore
+			</Link>
+			<Link
+				href="/"
+				className="text-sm px-7 py-4 hover:bg-blue-300 hover:text-zinc-900 font-extralight h-full"
+			>
+				Github
+			</Link>
+
+			<button className="text-sm px-7 py-3 rounded-full bg-blue-300  font-extralight h-full">
+				Sign Up
+			</button>
+		</nav>
+	);
+};
+
+export default Navbar;
