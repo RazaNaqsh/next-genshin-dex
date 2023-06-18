248a034d2aade80209e028c1eff98a95ec759dcb
RazaNaqsh <owaisraza1704@gmail.com>
2023-06-18 19:43:54 +0530

    Add meta data

---
+++ src/app/dex/characters/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const metadata = {
+	title: "Dex - Characters",
+	description: "A Genshin Dex App Made with Next13",
+};
+
+const page = () => {
+	return <div>page</div>;
+};
+
+export default page;
---
+++ src/app/dex/weapons/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const metadata = {
+	title: "Dex - Weapons",
+	description: "A Genshin Dex App Made with Next13",
+};
+
+const page = () => {
+	return <div>page</div>;
+};
+
+export default page;
