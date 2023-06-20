fa2729b5385c2c3e702dbec9362aca736b5ee58b
RazaNaqsh <owaisraza1704@gmail.com>
2023-06-20 20:07:03 +0530

    Better card layout and dynamic pages

--- src/app/dex/characters/page.tsx
+++ src/app/dex/characters/page.tsx
@@ -1,7 +1,8 @@
 "use client";
-import { useQuery } from "@tanstack/react-query";
+import { isError, useQuery } from "@tanstack/react-query";
 import { charProps } from "@/types";
 import Image from "next/image";
+import Link from "next/link";
 
 async function getCharacters() {
 	const res = await fetch("https://genshinlist.com/api/characters");
@@ -10,33 +11,41 @@ async function getCharacters() {
 }
 
 const page = () => {
-	const { data, isLoading, error } = useQuery({
+	const { data, isLoading, isError, error } = useQuery({
 		queryKey: ["characters"],
 		queryFn: () => getCharacters(),
 	});
 	console.log(data);
+	if (isLoading) {
+		return <span>Loading....</span>;
+	}
+	if (isError) {
+		<span>Error...</span>;
+	}
 	return (
 		<div className="container m-auto py-10">
 			<h1 className="text-4xl text-center">Character List</h1>
 			<div className="grid grid-cols-4 gap-4">
 				{data &&
 					data.map((char: charProps) => (
-						<div
-							id={char.id}
-							className="h-[200px] w-[250px] bg-blue-400 relative"
-						>
-							<div className="flex">
-								<Image
-									src={`https://genshinlist.com/assets/img/characters/${char.name.toLowerCase()}.png`}
-									alt={char.name}
-									fill
-									className="antialiased object-contain"
-								/>
-								<h3>{char.name}</h3>
+						<Link href={`dex/characters/${char.id}`}>
+							<div
+								id={char.id}
+								className="h-[350px] w-[300px] bg-blue-400 rounded-lg relative z-[-10]"
+							>
+								<div className="flex">
+									<Image
+										src={`https://genshinlist.com/assets/img/characters/${char.name.toLowerCase()}.png`}
+										alt={char.name}
+										height={70}
+										width={70}
+										className="antialiased object-contain"
+									/>
+									<h3 className="z-10 text-white">{char.name}</h3>
+								</div>
+								{/* https://genshinlist.com/assets/img/characters/diluc.png */}
 							</div>
-
-							{/* https://genshinlist.com/assets/img/characters/diluc.png */}
-						</div>
+						</Link>
 					))}
 			</div>
 		</div>
--- src/app/dex/weapons/page.tsx
+++ src/app/dex/weapons/page.tsx
@@ -2,6 +2,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { weaponProps } from "@/types";
 import Image from "next/image";
+import Link from "next/link";
 
 async function getWeapons() {
 	const res = await fetch("https://genshinlist.com/api/weapons");
@@ -10,31 +11,42 @@ async function getWeapons() {
 }
 
 const page = () => {
-	const { data, isLoading, error } = useQuery({
+	const { data, isLoading, isError, error } = useQuery({
 		queryKey: ["weapons"],
 		queryFn: () => getWeapons(),
 	});
 	console.log(data);
+	if (isLoading) {
+		return <span>Loading....</span>;
+	}
+	if (isError) {
+		<span>Error...</span>;
+	}
 	return (
 		<div className="container m-auto py-10">
 			<h1 className="text-4xl text-center">Weapon List</h1>
 			<div className="grid grid-cols-4 gap-4">
 				{data &&
 					data.map((weapon: weaponProps) => (
-						<div
-							id={weapon.id}
-							className="h-[200px] w-[250px] bg-green-400 relative"
-						>
-							<Image
-								src={`https://genshinlist.com/assets/img/weapons/${weapon.name
-									.toLowerCase()
-									.replaceAll(" ", "-")}.png`}
-								alt={weapon.name}
-								fill
-								className="antialiased object-contain z-0"
-							/>
-							<h3 className="z-10">{weapon.name}</h3>
-						</div>
+						<Link href={`dex/weapons/${weapon.id}`}>
+							<div
+								id={weapon.id}
+								className="h-[350px] w-[300px] bg-green-400 rounded-lg relative z-[-10]"
+							>
+								<div className="flex">
+									<Image
+										src={`https://genshinlist.com/assets/img/weapons/${weapon.name
+											.toLowerCase()
+											.replaceAll(" ", "-")}.png`}
+										alt={weapon.name}
+										height={70}
+										width={70}
+										className="antialiased object-cover"
+									/>
+									<h3 className="z-10 text-white">{weapon.name}</h3>
+								</div>
+							</div>
+						</Link>
 					))}
 			</div>
 		</div>
