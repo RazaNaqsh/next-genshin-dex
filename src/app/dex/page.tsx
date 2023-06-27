e0ec7b299942532722e3b9cef1bdedcaafa6a7b4
RazaNaqsh <owaisraza1704@gmail.com>
2023-06-27 23:07:33 +0530

    Char and weapon card bg

--- src/app/dex/page.tsx
+++ src/app/dex/page.tsx
@@ -1,3 +1,5 @@
+import { url } from "inspector";
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
@@ -8,14 +10,26 @@ export const metadata = {
 
 const page = () => {
 	return (
-		<div className="py-12 flex-center gap-28 h-[80vh]">
+		<div className="py-12 flex-center gap-28 h-[80vh] text-white">
 			<Link href="/dex/characters">
-				<div className="card h-[400px] w-[300px] rounded-3xl bg-blue-300 flex items-end p-6 justify-center">
+				<div
+					className="card h-[400px] w-[300px] relative rounded-3xl bg-blue-300 flex items-end p-6 justify-center"
+					style={{
+						backgroundImage: ` url(/genshinChars.avif)`,
+						backgroundPosition: "top",
+					}}
+				>
 					<h3 className="text-2xl">Characters</h3>
 				</div>
 			</Link>
 			<Link href="/dex/weapons">
-				<div className="card h-[400px] w-[300px] rounded-3xl bg-green-300 flex items-end p-6 justify-center">
+				<div
+					className="card h-[400px] w-[300px] rounded-3xl bg-green-300 flex items-end p-6 justify-center"
+					style={{
+						backgroundImage: ` url(/genshinWeapons.webp)`,
+						backgroundPosition: "center",
+					}}
+				>
 					<h3 className="text-2xl">Weapons</h3>
 				</div>
 			</Link>
