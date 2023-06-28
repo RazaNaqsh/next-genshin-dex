0054bf675a43587a16b0c39c944bf6f456d3493b
RazaNaqsh <owaisraza1704@gmail.com>
2023-06-28 12:31:16 +0530

    Fix about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const page = () => {
-	return <div>page</div>;
+const About = () => {
+	return <div>This is about page</div>;
 };
 
-export default page;
+export default About;
