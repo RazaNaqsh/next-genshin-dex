941c68b5dd7b50c78ac87396f1bc4f99544e4b18
RazaNaqsh <owaisraza1704@gmail.com>
2023-06-28 11:52:09 +0530

    Get individual data for dynamic routes

--- src/app/dex/characters/[char]/page.tsx
+++ src/app/dex/characters/[char]/page.tsx
@@ -1,13 +1,55 @@
 "use client";
-import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import Image from "next/image";
+import React, { useState } from "react";
 
-const page = ({ params }: any) => {
-	
-	const { char } = params;
+interface Props {
+	params: {
+		char: number;
+	};
+}
 
-	console.log(char);
+async function getCharacters() {
+	const res = await fetch("https://genshinlist.com/api/characters");
+	const characters = await res.json();
+	return characters;
+}
 
-	return <div>char page :{char}</div>;
+const page = ({ params }: Props) => {
+	const { data, isLoading, isError, error } = useQuery({
+		queryKey: ["characters"],
+		queryFn: () => getCharacters(),
+	});
+
+	if (data) console.log(data[params.char]);
+	if (isLoading) {
+		return <span>Loading....</span>;
+	}
+	if (isError) {
+		<span>Error...</span>;
+	}
+
+	const charData = data[params.char];
+	console.log(charData);
+	return (
+		<div className="container">
+			<div>
+				<Image
+					src={`https://genshinlist.com/assets/img/characters/${charData.name.toLowerCase()}.png`}
+					alt={charData.name}
+					height={70}
+					width={70}
+					className="w-[90px] h-[90px] antialiased object-contain rounded-full shadow-xl"
+				/>
+				<p>{data[params.char].name}</p>
+				<p>Rarity: {charData.rarity}</p>
+				<p>Birthday : {charData.birthday} </p>
+				<p>Vision : {charData.vision} </p>
+				<p>Weapon : {charData.weapon} </p>
+				<p>Desc : {charData.description}</p>
+			</div>
+		</div>
+	);
 };
 
 export default page;
